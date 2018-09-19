c103051976dcd5ed04355192682de6890d77b7dc
Alexis <alexisaubert8109@outlook.fr>
2018-09-19 15:33:19 +0200

    add branch and components ArticleList

---
+++ Components/ArticleListScreen/ArticleListScreen.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+
+export default class ArticleListScreen extends Component {
+    render() {
+        return (
+          <View>
+            <Text>List actus</Text>
+          </View>
+        );
+      }
+}
