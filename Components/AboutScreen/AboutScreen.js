daaae89002220048a8dfd468f0fcd1e048d91c6f
Kilian <killempe@sfr.fr>
2018-09-20 14:36:23 +0200

    Ajout du A propos

--- Components/AboutScreen/AboutScreen.js
+++ Components/AboutScreen/AboutScreen.js
@@ -2,21 +2,24 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View,TouchableOpacity, ToastAndroid} from 'react-native';
 
 
-let SoundPlayer = require('react-native-sound');
+import Sound from 'react-native-sound';
 
-let song = null;
+// const song = new Sound('tuturu.mp3', SoundPlayer.MAIN_BUNDLE, (error) =>{
+//         if(error) ToastAndroid.show('Error to load the sound :' + error, ToastAndroid.LONG);
+//         else ToastAndroid.show('Duration : '+ song.getDuration() + "; num of channels : " + song.getNumberOfChannels, ToastAndroid.LONG);
+//     });
 
 export default class AboutScreen extends Component {
 
     constructor(props){
-        super(props)
-
+        super(props);
+        Sound.setCategory('Playback');
         this.state ={
             pause: false,
         };
     }
 
-    componentWillMount(){
+    componentDidMount(){
         // song= new SoundPlayer('tuturu.mp3', SoundPlayer.MAIN_BUNDLE, (error) =>{
         //     if(error)
         //     ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
@@ -25,19 +28,27 @@ export default class AboutScreen extends Component {
     }
 
     onPressButtonPlay(){
-        song= new SoundPlayer('tuturu.mp3', SoundPlayer.MAIN_BUNDLE, (error) =>{
-            if(error)
-            ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
-            
-            else{
-                (song != null)
-                    song.play((success) =>{
-                        if(!success)
-                        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT);
-                    });
-                   
-            }
+        // this.setState({song : 
+        // });
+        this.song = new Sound('tuturu.mp3', Sound.MAIN_BUNDLE, (error) => {
+            ToastAndroid.show(error, ToastAndroid.SHORT);
+            if(error) ToastAndroid.show('Error to load the sound ', ToastAndroid.LONG);
+            else ToastAndroid.show('Duration : '+ song.getDuration() + "; num of channels : " + song.getNumberOfChannels, ToastAndroid.LONG);
         });
+        ToastAndroid.show('' + this.song.getNumberOfChannels(), ToastAndroid.SHORT);
+        // song= new SoundPlayer('tuturu.mp3', SoundPlayer.MAIN_BUNDLE, (error) =>{
+        //     if(error)
+        //     ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+            
+        //     else{
+        //         (song != null)
+        //             song.play((success) =>{
+        //                 if(!success)
+        //                 ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT);
+        //             });
+                   
+        //     }
+        // });
         // if(song != null){
         //     song.play((success) =>{
         //         if(!success)
@@ -68,11 +79,11 @@ export default class AboutScreen extends Component {
             <Text style={styles.name}>- Lempereur Kilian</Text>
             <Text style={styles.name}>- Grandpierre Adrien</Text>
             <Text style={styles.name}>- Aubert Alexis</Text>
-            <TouchableOpacity onPress={this.onPressButtonPlay.bind(this)}>
-            <Text>YOLO</Text>
+            <TouchableOpacity style={styles.button} onPress={this.onPressButtonPlay.bind(this)}>
+                <Text style={{color: 'white'}}>YOLO</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={this.onPressButtonPause.bind(this)}>
-            <Text>{this.state.pause ? 'RESUME' : 'STOP'}</Text>
+            <TouchableOpacity style={styles.button} onPress={this.onPressButtonPause.bind(this)}>
+                <Text style={{color: 'white'}} >{this.state.pause ? 'RESUME' : 'STOP'}</Text>
             </TouchableOpacity> 
           </View>
         );
@@ -99,5 +110,14 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: '#000000',
         marginBottom: 10,
+    },
+    button: {
+        marginTop: 30,
+        borderColor: '#000000',
+        borderWidth: 1,
+        padding: 8,
+        backgroundColor: '#660000',
+        borderRadius: 5,
+        borderColor: '#000000',
     }
 });
